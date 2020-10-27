3996487c91ff529b3eca68dcb516a081063b902b
Adriel Izoton de Faci <adriel.faci@promob.com>
2020-10-27 17:41:18 -0300

    website almost done

--- baseCSS/src/js/default.js
+++ baseCSS/src/js/default.js
@@ -1,2 +1,13 @@
 // Default Javascript File
 console.log('Base starter, ready to rock and roll!');
+
+$("#btnToggleLearnList").click(function () {
+   if( $("#learnList").css("display") === "block"){
+       $("#learnList").css("display", "none");
+       $("#btnToggleLearnList").text("show");
+    }
+   else{
+       $("#learnList").css("display", "block");
+       $("#btnToggleLearnList").text("hide");
+   }
+})
